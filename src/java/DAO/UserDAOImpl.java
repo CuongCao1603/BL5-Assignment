e312f78b757456ad59c4364a516a131047a51903
CuongCao1603 <caovuong566@gmail.com>
2023-04-19 00:50:39 +0700

    init project

--- src/java/DAO/UserDAOImpl.java
+++ src/java/DAO/UserDAOImpl.java
@@ -28,8 +28,12 @@ public class UserDAOImpl {
 //    }
 
 //   
-    public void userRegister(String name,String email,String phno,String password) {
-        String sql = "insert into [user] values(?,?,?,?,?,?,?,?,?)";
+    public void userRegister(String name, String email, String phno, String password) {
+        String sql = "insert into [user] ([name]\n" +
+"           ,[email]\n" +
+"           ,[phno]\n" +
+"           ,[password])"
+                + " values(?,?,?,?)";
 //        boolean f = false;
         try {
 
@@ -39,17 +43,47 @@ public class UserDAOImpl {
             ps.setString(2, email);
             ps.setString(3, phno);
             ps.setString(4, password);
-            ps.setString(5, "NULL");
-            ps.setString(6, "NULL");
-            ps.setString(7, "NULL");
-            ps.setString(8, "NULL");
-            ps.setString(9, "NULL");
+//            ps.setString(5, "NULL");
+//            ps.setString(6, "NULL");
+//            ps.setString(7, "NULL");
+//            ps.setString(8, "NULL");
+//            ps.setString(9, "NULL");
 
             ps.executeUpdate();
             System.err.println("tai len data");
         } catch (Exception e) {
         }
-        
+
+    }
+
+    public User login(String email, String password) {
+        User us=null;
+        String sql = "select * from [user] \n"
+                + "where [email] = ?\n"
+                + "and [password] = ?";
+        try {
+            conn = new DBConnect().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+//                us=new User();
+                return new User(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getString(10));
+            }
+        } catch (Exception e) {
+        }
+        return null;
     }
 
 }
--- web/login.jsp
+++ web/login.jsp
@@ -4,6 +4,7 @@
     Author     : Admin
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -20,15 +21,20 @@
                     <div class="card">
                         <div class="card-body">
                             <h3 class="text-center">Login</h3>
-                            <form>
+                            <c:if test="${not empty failedMsg}">
+                                <h5 class="text-center text-danger">${failedMsg}</h5>
+                                <c:remove var="failedMsg" scope="session"/>
+                            </c:if>
+                            
+                            <form action="login" method="post">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Email address</label>
                                     <input type="email" class="form-control" 
-                                           id="exampleInputEmail1" aria-describedby="emailHelp" required="">
+                                           id="exampleInputEmail1" aria-describedby="emailHelp" required="" name="email">
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" required="">
+                                    <input type="password" class="form-control" id="exampleInputPassword1" required="" name="password">
                                 </div>
 <!--                                <div class="form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
